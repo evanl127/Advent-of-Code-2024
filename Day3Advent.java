ca3719bdb87652a40960908f85d2eb381404d73a
evanl127 <144394321+evanl127@users.noreply.github.com>
2024-12-06 13:47:43 -0500

    Add files via upload

--- Day3Advent.java
+++ Day3Advent.java
@@ -9,9 +9,12 @@ public class Day3Advent {
 
         ArrayList<String> fileData = getFileData("src/Day3Input");
         ArrayList<String> combined = new ArrayList<>();
+        StringBuilder concan = new StringBuilder(new String());
         for(String i : fileData)
-            combined.add(i);
-        mulTimes(combined);
+            concan.append(i);
+        combined.add(concan.toString());
+        System.out.println(mulTimes(combined));
+        System.out.println(mulTimes2(combined));
     }
 
     public static ArrayList<String> getFileData(String fileName) {
@@ -32,22 +35,46 @@ public class Day3Advent {
     public static int mulTimes(ArrayList<String> a){
         int total = 0;
         ArrayList<String> allMatches = new ArrayList<>();
-        String regex = "mul\\([1-9][0-9]{0,2},[1-9][0,9]{0,2}\\)";
+        String regex = "mul\\([1-9][0-9]{0,2},[1-9][0-9]{0,2}\\)";
         for(String i : a) {
             Matcher m = Pattern.compile(regex).matcher(i);
             while (m.find()) {
                 allMatches.add(m.group());
             }
         }
-        ArrayList<String> noMul = new ArrayList<>();
         for(String j : allMatches){
-            String[] mulLess = j.split("mul");
-            ArrayList<String> mulLessAL = new ArrayList<>();
-            for(String k : mulLess){
-                mulLessAL.add(k);
+            String h = j.substring(3);
+            int k = h.indexOf(",");
+            int numberOne = Integer.parseInt(h.substring(1,k));
+            int numberTwo = Integer.parseInt(h.substring(k+1,h.length()-1));
+            total += numberTwo * numberOne;
+        }
+        return total;
+    }
+    public static int mulTimes2(ArrayList<String> a){
+        int total = 0;
+        ArrayList<String> allMatches = new ArrayList<>();
+        String regex = "mul\\([1-9][0-9]{0,2},[1-9][0-9]{0,2}\\)|do\\(\\)|don't\\(\\)";
+        for(String i : a) {
+            Matcher m = Pattern.compile(regex).matcher(i);
+            while (m.find()) {
+                allMatches.add(m.group());
             }
-            for(int l = mulLessAL; l < mulLessAL.size(); l-=2) {
-
+        }
+        boolean count = true;
+        for(String j : allMatches){
+            if(j.equals("don't()")){
+                count = false;
+            }
+            else if(j.equals("do()")){
+                count=true;
+            }
+            else if(count) {
+                String h = j.substring(3);
+                int k = h.indexOf(",");
+                int numberOne = Integer.parseInt(h.substring(1, k));
+                int numberTwo = Integer.parseInt(h.substring(k + 1, h.length() - 1));
+                total += numberTwo * numberOne;
             }
         }
         return total;
