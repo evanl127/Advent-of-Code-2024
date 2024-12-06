2d6cec8ef639c26a77fbc36fbe868a692ffa0e24
evanl127 <144394321+evanl127@users.noreply.github.com>
2024-12-06 14:40:54 -0500

    Add files via upload

--- Day4Advent.java
+++ Day4Advent.java
@@ -8,10 +8,7 @@ public class Day4Advent {
     public static void main(String[] args) {
 
         ArrayList<String> fileData = getFileData("src/Day4Input");
-        ArrayList<String> test = new ArrayList<>();
-        test.add("MXMXAXMASX");
-        test.add("XMASAMXAMM");
-        System.out.println(xmasCount(test));
+        System.out.println(xmasCount(fileData));
     }
 
     public static ArrayList<String> getFileData(String fileName) {
@@ -30,33 +27,28 @@ public class Day4Advent {
         }
     }
     public static int xmasCount(ArrayList<String> fileData){
-        int total = 0;
-        total = toRight(fileData) + toLeft(fileData) + toUp(fileData);
+        int total;
+        total = toRight(fileData) + toUp(fileData) + diagonalDown(fileData) + diagonalUp(fileData);
         return total;
     }
     public static int toRight(ArrayList<String> fileData){
         int count = 0;
         for(String i : fileData){
-            for(int j = 0; j < i.length() - 4; j++){
-                if(i.substring(j,j+4).equals("XMAS"))
+            for(int j = 0; j < i.length() - 3; j++){
+                if(i.substring(j,j+4).equals("XMAS")) {
                     count++;
-            }
-        }
-        return count;
-    }
-    public static int toLeft(ArrayList<String> fileData){
-        int count = 0;
-        for(String i : fileData){
-            for(int j = 0; j < i.length() - 4; j++){
-                if(i.substring(j,j+4).equals("SAMX"))
+                }
+                else if(i.substring(j,j+4).equals("SAMX")){
                     count++;
+                }
+
             }
         }
         return count;
     }
     public static int toUp(ArrayList<String> fileData) {
         int count = 0;
-        for (int i = 0; i < fileData.size() - 4; i++) {
+        for (int i = 0; i < fileData.size() - 3; i++) {
             for (int j = 0; j < fileData.get(0).length(); j++) {
                 String combined = "";
                 combined += fileData.get(i).substring(j, j + 1);
@@ -65,23 +57,46 @@ public class Day4Advent {
                 combined += fileData.get(i + 3).substring(j, j + 1);
                 if (combined.equals("XMAS"))
                     count++;
+                else if(combined.equals("SAMX"))
+                    count++;
             }
         }
         return count;
     }
-//    public static int toDown(ArrayList<String> fileData){
-//
-//    }
-//    public static int toLeftDown(ArrayList<String> fileData){
-//
-//    }
-//    public static int toLeftUp(ArrayList<String> fileData){
-//
-//    }
-//    public static int toRightUp(ArrayList<String> fileData){
-//
-//    }
-//    public static int toRightDown(ArrayList<String> fileData){
-//
-//    }
+
+    public static int diagonalDown(ArrayList<String> fileData){
+        int count = 0;
+        for (int i = 0; i < fileData.size() - 3; i++){
+            for (int j = 3; j < fileData.get(0).length(); j++) {
+                String combined = "";
+                combined += fileData.get(i).substring(j, j + 1);
+                combined += fileData.get(i + 1).substring(j - 1, j);
+                combined += fileData.get(i + 2).substring(j - 2, j - 1);
+                combined += fileData.get(i + 3).substring(j - 3, j - 2);
+                if (combined.equals("XMAS"))
+                    count++;
+                else if(combined.equals("SAMX"))
+                    count++;
+            }
+        }
+        return count;
+    }
+    public static int diagonalUp(ArrayList<String> fileData){
+        int count = 0;
+        for (int i = 3; i < fileData.size(); i++){
+            for (int j = 0; j < fileData.get(0).length() - 3; j++) {
+                String combined = "";
+                combined += fileData.get(i).substring(j, j + 1);
+                combined += fileData.get(i - 1).substring(j + 1, j + 2);
+                combined += fileData.get(i - 2).substring(j + 2, j + 3);
+                combined += fileData.get(i - 3).substring(j + 3, j + 4);
+                if (combined.equals("XMAS"))
+                    count++;
+                else if(combined.equals("SAMX"))
+                    count++;
+            }
+        }
+        return count;
+    }
+
 }
